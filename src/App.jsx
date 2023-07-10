6ec6ac33970f3913c510ddab1c1f66be8a5ee6c3
jaki010789 <135663461+jaki010789@users.noreply.github.com>
2023-07-10 16:35:59 -0600

    commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function App() {
+  return (
+   
+      <header className="App-header">
+      <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home"></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">REACT-NAVBAR</Nav.Link>
+            <Nav.Link href="#link">Inicio</Nav.Link>
+            <NavDropdown title="menú principal" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">links</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                imágenes
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">páginas</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+      </header>
+  
+  );
+}
+
+export default App;
