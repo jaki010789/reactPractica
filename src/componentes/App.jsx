2398d8108eb3b10803cb4b5833e86fe697985eda
jaki010789 <135663461+jaki010789@users.noreply.github.com>
2023-07-11 10:54:35 -0600

    practica componentes

--- src/componentes/App.jsx
+++ src/componentes/App.jsx
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import  Cerrarsesión from "./index";
 
 function App() {
   return (
@@ -27,6 +28,8 @@ function App() {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
+
+            <Cerrarsesión></Cerrarsesión>
           </Nav>
         </Navbar.Collapse>
       </Container>
---
+++ src/componentes/index.jsx
@@ -0,0 +1,25 @@
+import React, { useState }  from "react";
+
+const CerrarSesion= () => {
+    const [inicio, escogerInicioSesion]= useState(false);
+
+    const DarInicio = () => {
+escogerInicioSesion(true);
+    };
+
+    const DarFinalizar= () => {
+        escogerInicioSesion(false);
+    };
+
+    return(
+        <div>
+            {inicio ? (
+                <button className="btn" onClick={DarFinalizar}> Cerrar sesión</button>):(
+              <button className="btn" onClick={DarInicio}>Iniciar sesión</button>
+            )}
+
+        </div>
+    );
+};
+
+export default CerrarSesion;
